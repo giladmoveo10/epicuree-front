f4a9e8702adf557ccec642d1a90bc0aa891746a5
Gil-ad Katz <gil-adk@moveo.co.il>
2023-10-03 22:00:10 +0300

    - added home page - added assets

---
+++ src/pages/homePage/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../../shared/components/Header/Header";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>asd</h1>
+            <Header />
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/shared/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+import "./Header.scss";
+// import MenuButton from "./components/MenuButton/MenuButton";
+// import LogoHeader from "./components/Logo/LogoHeader";
+// import IconsGroup from "./components/IconsGroup/IconsGroup";
+
+const Header = () => {
+    // const handleMenuClick = () => {
+    //     console.log("Menu button clicked");
+    // };
+
+    return (
+        <header className="header">
+            {/* <MenuButton onMenuClick={handleMenuClick}></MenuButton> */}
+            {/* <LogoHeader></LogoHeader>
+            <IconsGroup></IconsGroup> */}
+        </header>
+    );
+};
+
+export default Header;
