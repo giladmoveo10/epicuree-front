baa3ec32b623e425dc60c2d60df45bb9fb472be8
Gil-ad Katz <gil-adk@moveo.co.il>
2023-10-05 17:15:14 +0300

    - insert the search picture section - fixed the nav header to be sticky

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -2,6 +2,7 @@ import "./HomePage.scss";
 import Header from "../../shared/components/Header/Header";
 import Footer from "../../shared/components/Footer/Footer";
 import AboutUs from "./components/AboutUs/AboutUs";
+import IntroSearch from "./components/IntroSearch/IntroSearch";
 // import homePageConstants from "./constants/homePageConstants";
 
 // import { useMediaQuery } from "react-responsive";
@@ -10,6 +11,7 @@ const Home = () => {
     return (
         <div className="homePage">
             <Header />
+            <IntroSearch />
             <div className="home-page-content">
                 <AboutUs />
             </div>
