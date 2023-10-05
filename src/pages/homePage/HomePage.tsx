f982a968ebcf3c4f00b45f6f9d880c23761f066d
Gil-ad Katz <gil-adk@moveo.co.il>
2023-10-05 15:10:48 +0300

    - fixed about me responsive to one component - removed about us desktop component

--- src/pages/homePage/HomePage.tsx
+++ src/pages/homePage/HomePage.tsx
@@ -2,24 +2,17 @@ import "./HomePage.scss";
 import Header from "../../shared/components/Header/Header";
 import Footer from "../../shared/components/Footer/Footer";
 import AboutUs from "./components/AboutUs/AboutUs";
-import homePageConstants from "./constants/homePageConstants";
-import AboutUsDesktop from "./components/AboutUsDesktop/AboutUsDesktop";
+// import homePageConstants from "./constants/homePageConstants";
 
-import { useMediaQuery } from "react-responsive";
+// import { useMediaQuery } from "react-responsive";
 
 const Home = () => {
-    const isDesktop = useMediaQuery({ minWidth: homePageConstants.desktopBreakpoint });
-    const isMobile = useMediaQuery({ maxWidth: homePageConstants.desktopBreakpoint });
-
     return (
         <div className="homePage">
             <Header />
-            {/* {isDesktop ? <AboutUsDesktop /> : <AboutUs />} */}
-
-            {isDesktop && <AboutUsDesktop />}
-            {/* {isMobile && <AboutUs />} */}
-            {/* <AboutUs></AboutUs> */}
-            {isMobile && <p>asd</p>}
+            <div className="home-page-content">
+                <AboutUs />
+            </div>
             <Footer />
         </div>
     );
--- src/pages/homePage/components/AboutUs/AboutUs.tsx
+++ src/pages/homePage/components/AboutUs/AboutUs.tsx
@@ -8,12 +8,20 @@ import homePageConstants from "../../constants/homePageConstants";
 function AboutUs() {
     return (
         <div className="about-us-container">
-            <img src={AboutLogo} alt="about logo" className="epicure-logo" />
-            <img src={AppStore} alt="App Store" className="app-store" />
-            <img src={GooglePlay} alt="Google Play" className="google-play" />
-            <h2>ABOUT US:</h2>
-            <p className="body-text about-us-text">{homePageConstants.aboutUsText}</p>
-            <p className="body-text about-us-text-2">{homePageConstants.aboutUsText2}</p>
+            <div>
+                <img src={AboutLogo} alt="about logo" className="epicure-logo" />
+            </div>
+            <div className="about-us-content">
+                <div className="stores">
+                    <img src={AppStore} alt="App Store" className="app-store" />
+                    <img src={GooglePlay} alt="Google Play" className="google-play" />
+                </div>
+                <div>
+                    <h2>ABOUT US:</h2>
+                    <p className="body-text about-us-text">{homePageConstants.aboutUsText}</p>
+                    <p className="body-text about-us-text-2">{homePageConstants.aboutUsText2}</p>
+                </div>
+            </div>
         </div>
     );
 }
