bc9f89107689cc3740a2d69444e6b9b4e8826321
Gil-ad Katz <gil-adk@moveo.co.il>
2023-10-09 15:53:47 +0300

    - added responsiveness to the chefOfTheWeek component

--- src/pages/homePage/components/ChefOfTheWeek/ChefOfTheWeek.tsx
+++ src/pages/homePage/components/ChefOfTheWeek/ChefOfTheWeek.tsx
@@ -6,15 +6,19 @@ import AllRestaurants from "../AllRestaurants/AllRestaurants";
 const ChefOfTheWeek: React.FC = () => {
     return (
         <div className="chef-container">
-            <h2>CHEF OF THE WEEK:</h2>
-            <div className="chef-info">
-                <div className="chef-name-wrapper">
-                    <h3>
-                        {chefOfTheWeek.chefFirstName} {chefOfTheWeek.chefLastName}
-                    </h3>
-                </div>
+            <div className="chef-title">
+                <h2>CHEF OF THE WEEK:</h2>
             </div>
-            <span className="body-text description">{chefOfTheWeek.description}</span>
+            <div className="about-chef-section">
+                <div className="chef-info">
+                    <div className="chef-name-wrapper">
+                        <h3>
+                            {chefOfTheWeek.chefFirstName} {chefOfTheWeek.chefLastName}
+                        </h3>
+                    </div>
+                </div>
+                <span className="body-text description">{chefOfTheWeek.description}</span>
+            </div>
             <h4 className="chef-restaurants">{chefOfTheWeek.chefFirstName}'S RESTAURANTS</h4>
             <div className="restaurants">
                 <Carusel items={chefOfTheWeek.restaurants} />
